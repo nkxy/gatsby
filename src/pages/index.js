5cc99b9b19323f5bc428a717addb562cb43bbc74
Nick Yang <nick.yang3721@gmail.com>
2018-11-10 14:23:33 -0500

    :bento: Update Landing Content

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "./../components/layout";
 export default () => (
   <Layout>
     <div>
-      <h1>Hello! My name is Nick. Nice to meet you.</h1>
+      <h1>Welcome! 😛</h1>
     </div>
   </Layout>
 );
